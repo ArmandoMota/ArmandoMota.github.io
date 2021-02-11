227edebcdbc68dd45054e460b5d847852db17645
ArmandoMota <armandomotagt@gmail.com>
2021-02-11 13:20:51 -0800

    Updates

---
+++ static/js/main.9f426ce2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),i=n.n(s),o=(n(8),n.p,n(0));var d=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h2",{children:"Currently Under Construction..."})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9f426ce2.chunk.js.map
